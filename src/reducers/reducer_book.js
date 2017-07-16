0ff61b55d345fb29813c268197418fc78bb742c7
fredchen90 <fredchen90@gmail.com>
2017-07-16 22:00:59 +0800

    Implementation of basic book deatl and basic redux cycle.

--- src/reducers/reducer_book.js
+++ src/reducers/reducer_book.js
@@ -1,8 +1,8 @@
 export default function() {
 	return [
-		{ title: 'The Mist'},
-		{ title: '12 MONKEYS'},
-		{ title: 'SHERLOCK'},
-		{ title: 'Javascript: the Good Parts'},
+		{ title: 'The Mist', page: 354},
+		{ title: '12 MONKEYS', page: 165},
+		{ title: 'SHERLOCK', page: 95},
+		{ title: 'Javascript: the Good Parts', page: 35},
 	]
 }
